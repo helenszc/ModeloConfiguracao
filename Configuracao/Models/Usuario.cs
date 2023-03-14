fa997f7fc428ec3d832b66a70a17f0b9fd7aaad8
helen coutinho <helencoutinhos@gmail.com>
2023-03-14 16:15:27 -0300

    mapeamento de tabelas na models

---
+++ Configuracao/Models/Usuario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string NomeUsuario { get; set; }
+        public string CPF { get; set; } 
+        public string Email { get; set; }
+        public bool Ativo { get; set; }
+        public string Senha { get; set; } 
+        public List<GrupoUsuario> GruposUsuarios { get; set; }
+    }
+}

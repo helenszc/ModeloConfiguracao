810db9ad023e3f38de8e43d63e4c17072ea7fe70
helen coutinho <helencoutinhos@gmail.com>
2023-03-14 16:35:33 -0300

    listas e métodos do UsuarioDALL

---
+++ Configuracao/DAL/UsuarioDAL.cs
@@ -0,0 +1,41 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class UsuarioDAL
+    {
+        public void Inserir(Usuario _usuario)
+        {
+
+        }
+
+        public List<Usuario> BuscarTodos()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Usuario> BuscarPorNomeUsuario(string _nomeUsuario)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Usuario> BuscarPorId(int _id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Atualizar(UsuarioDAL _usuario)
+        {
+
+        }
+        public void Excluir(int _id)
+        {
+
+        }
+    }
+}

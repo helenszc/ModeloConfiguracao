bd4662df6ca3eecc1be94bec926bd3a526395deb
helen coutinho <helencoutinhos@gmail.com>
2023-03-16 16:01:52 -0300

    BLL
    Criação das classes UsuarioBLL, GrupoUsuarioBLL, PermissaoBLL e implementação dos métodos e buscas na UsuarioBLL

---
+++ Configuracao/BLL/UsuarioBLL.cs
@@ -0,0 +1,39 @@
+﻿
+using DAL;
+using Models;
+using System;
+
+namespace BLL
+{
+    public class UsuarioBLL
+    {
+        public void Inserir(Usuario _usuario)
+        {
+            ValidarDados(_usuario);
+
+            UsuarioDAL usuarioDAL = new UsuarioDAL();  
+            usuarioDAL.Inserir(_usuario);
+
+        }
+
+        public void Alterar(Usuario _usuario)
+        {
+            ValidarDados(_usuario);
+
+            UsuarioDAL usuarioDAL = new UsuarioDAL();
+            usuarioDAL.Alterar(_usuario);
+        }
+        private void ValidarDados(Usuario _usuario)
+        {
+            if (_usuario.Senha.Length <= 3)
+            {
+                throw new Exception("A senha deve ter mais de 3 caracteres");
+            }
+
+            if(_usuario.Nome.Length <= 2)
+            {
+                throw new Exception("O nome deve ter mais de 2 caracteres");
+            }
+        }
+    }
+}
